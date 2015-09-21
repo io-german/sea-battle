a392e3c50cefce047db61fc75678230f68327cda
ig <ig@anahoret.com>
2015-09-21 11:49:18 +0300

    Add detection of clicked cell.

--- app/assets/javascripts/game_field/main.js
+++ app/assets/javascripts/game_field/main.js
@@ -1,6 +1,7 @@
 /* eslint no-unused-vars: 0 */
 import Model from './model.js';
 import View from './view.js';
+import getLetter from '../util/letters.js';
 
 export default function () {
   var ownField       = document.getElementById('own-field'),
@@ -10,9 +11,21 @@ export default function () {
       own            = new Model(ownFieldView),
       rival          = new Model(rivalFieldView);
 
-  rivalField.addEventListener('click', function () {
-    console.log('click');
-  });
+  Array.prototype.forEach.call(
+    document.getElementsByTagName('canvas'),
+    (canvas) => canvas.addEventListener('click', callback));
+
+  function callback(e) {
+    var target = e.target,
+        targetRect = target.getBoundingClientRect(),
+        targetPosition = { x: Math.ceil(targetRect.left), y: Math.ceil(targetRect.top) },
+        clickPosition = { x: e.clientX - targetPosition.x, y: e.clientY - targetRect.top },
+        cell = { row: Math.floor(clickPosition.y / 25), col: getLetter(Math.floor(clickPosition.x / 25) - 1) };
+
+    if (cell.col && cell.row) {
+      console.log(cell.col + cell.row);
+    }
+  }
 
   ownFieldView.draw();
   rivalFieldView.draw();
--- app/assets/javascripts/game_field/view.js
+++ app/assets/javascripts/game_field/view.js
@@ -1,4 +1,5 @@
 /* eslint no-unused-vars: 0 */
+import getLetter from '../util/letters.js';
 
 export default class {
   constructor (canvas) {
@@ -13,12 +14,10 @@ export default class {
 }
 
 function drawMarkings (ctx) {
-  var letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k' ];
-
   ctx.font = '25px serif';
-  letters.forEach(function (letter, index) {
-    ctx.fillText(letter, 25 + index * 25, 25);
-  });
+  for (let i = 0; i < 10; i++) {
+    ctx.fillText(getLetter(i), 25 + i * 25, 25);
+  }
 
   for (let i = 0; i < 10; i++) {
     ctx.fillText(i + 1, 0, 50 + i * 25);
