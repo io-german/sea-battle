0196c5358027b68f31d85b026d638ed0c070b307
ig <ig@anahoret.com>
2015-09-21 14:07:13 +0300

    Use const expression for letters list.

--- app/assets/javascripts/util/letters.js
+++ app/assets/javascripts/util/letters.js
@@ -1,11 +1,11 @@
-var letters = [
-  'a', 'b', 'c', 'd', 'e',
-  'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o',
-  'p', 'q', 'r', 's', 't',
-  'u', 'v', 'w', 'x', 'y',
-  'z' ];
-
 export default function (number) {
+  const letters = [
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y',
+    'z' ];
+
   return letters[ number ];
 }
