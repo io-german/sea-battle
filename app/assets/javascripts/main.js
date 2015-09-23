59e17d056ea197f148c36847bc033ca63abb6ce4
ig <ig@anahoret.com>
2015-09-23 14:07:08 +0300

    Cleanup application entry point.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,5 @@
 import game_field from './game_field/game/main.js';
 import arrangement from './game_field/arrangement/main.js';
-import websocket from './websocket/main.js';
 
 game_field();
 arrangement();
-var socket = websocket('ws://localhost:9000/subscribe');
-
-setTimeout(() => socket.send('ololo'), 5);
-
-window.socket = socket;
