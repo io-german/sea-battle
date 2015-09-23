8976c5fecc4027c03550a05a4e07d97c64bb1686
ig <ig@anahoret.com>
2015-09-23 14:20:33 +0300

    Add processing of keyboard events in arrangement module.

--- app/assets/javascripts/game_field/arrangement/main.js
+++ app/assets/javascripts/game_field/arrangement/main.js
@@ -1,8 +1,32 @@
+import * as key_codes from '../../util/key_codes.js';
 import view from './view.js';
 import Model from './model.js';
 
 export default function () {
-  var a = new Model(view);
+  var model = new Model(view);
 
-  console.log(a);
+  document.addEventListener('keyup', function (e) {
+    e.preventDefault();
+
+    switch (e.keyCode) {
+      case key_codes.LEFT_ARROW:
+        model.move('left');
+        break;
+      case key_codes.RIGHT_ARROW:
+        model.move('right');
+        break;
+      case key_codes.UP_ARROW:
+        model.move('up');
+        break;
+      case key_codes.DOWN_ARROW:
+        model.move('down');
+        break;
+      case key_codes.SPACE:
+        model.rotate();
+        break;
+      case key_codes.ENTER:
+        model.place();
+        break;
+    }
+  });
 }
