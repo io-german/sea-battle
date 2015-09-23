7522e247820fed2d637b7aeb5bf2e477b1578ccc
ig <ig@anahoret.com>
2015-09-23 14:31:29 +0300

    Replace jey_codes module with keycode NPM package.

--- app/assets/javascripts/game_field/arrangement/main.js
+++ app/assets/javascripts/game_field/arrangement/main.js
@@ -1,4 +1,4 @@
-import * as key_codes from '../../util/key_codes.js';
+import keycode from 'keycode';
 import view from './view.js';
 import Model from './model.js';
 
@@ -9,23 +9,14 @@ export default function () {
     e.preventDefault();
 
     switch (e.keyCode) {
-      case key_codes.LEFT_ARROW:
-        model.move('left');
-        break;
-      case key_codes.RIGHT_ARROW:
-        model.move('right');
-        break;
-      case key_codes.UP_ARROW:
-        model.move('up');
-        break;
-      case key_codes.DOWN_ARROW:
-        model.move('down');
-        break;
-      case key_codes.SPACE:
+      case keycode('space'):
         model.rotate();
         break;
-      case key_codes.ENTER:
+      case keycode('enter'):
         model.place();
+        break;
+      default:
+        model.move(keycode(e.keyCode));
         break;
     }
   });
