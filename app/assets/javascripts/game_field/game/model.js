828e2b1b2abdbec4f52eb4fc81236e686e3f1c48
ig <ig@anahoret.com>
2015-09-25 14:18:20 +0300

    Separate rivalShot and ownShot functions.

--- app/assets/javascripts/game_field/game/model.js
+++ app/assets/javascripts/game_field/game/model.js
@@ -1,4 +1,6 @@
 import createField from '../helper/create_field.js';
+import getShipCells from './get_ship_cells.js';
+import getShipNeighborCells from './get_ship_neighbor_cells.js';
 
 export default class {
   constructor (view, own, rival) {
@@ -11,10 +13,31 @@ export default class {
     this.view(this);
   }
 
-  shot (fieldName, coord, result) {
-    var field = this[fieldName];
+  rivalShot (coord) {
+    var field     = this.own,
+        cellValue = field[ coord.row ][ coord.col ],
+        result    = 'm';
 
-    field[coord.row][coord.col] = result;
-    this.view(this);
+    if (cellValue === 's') {
+      field[ coord.row ][ coord.col ] = 'w';
+
+      let cells      = getShipCells(field, coord),
+          shipKilled = cells.reduce((prev, curr) => prev && curr === 'w', true);
+
+      result = shipKilled ? 'k' : 'w';
+    }
+
+    return result;
+  }
+
+  ownShot (coord, result) {
+    var field = this.rival;
+
+    field[coord.row][coord.col] = result === 'k' ? 'w' : result;
+    if (result === 'k') {
+      let neighborCells = getShipNeighborCells(field, coord);
+
+      neighborCells.forEach((cell) => field[cell.row][cell.col] = 'm');
+    }
   }
 }
