841850db7b3d6adb6100189d2e6a29b2faf4377b
ig <ig@anahoret.com>
2015-09-29 17:23:01 +0300

    Implement game logic as finite state machine.

---
+++ app/fsa/states.scala
@@ -0,0 +1,76 @@
+package fsa
+
+import scala.util.Random
+
+sealed trait FiniteStateMachine
+sealed trait OperationVertex[I, O <: FiniteStateMachine] extends FiniteStateMachine {
+  def next(inputData: I): O
+}
+sealed trait ConditionalVertex[O1 <: FiniteStateMachine, O2 <: FiniteStateMachine] extends FiniteStateMachine {
+  def output1: O1
+  def output2: O2
+  def predicate: Boolean
+
+  def next =
+    if (predicate) output1
+    else output2
+}
+sealed trait EndVertex extends FiniteStateMachine
+
+case object InitialState extends OperationVertex[String, Player2Connection] {
+  override def next(player1: String): Player2Connection = Player2Connection(player1)
+}
+
+case class Player2Connection(player1: String) extends OperationVertex[String, ArrangementOp] {
+  override def next(player2: String): ArrangementOp = ArrangementOp(Seq(player1, player2))
+}
+
+case class ArrangementOp(players: Seq[String], player1Finished: Boolean = false, player2Finished: Boolean = false) extends OperationVertex[String, ArrangementCond] {
+  override def next(finishedPlayer: String): ArrangementCond = {
+    val player1 :: player2 :: _ = players
+
+    if (player1 == finishedPlayer) ArrangementCond(players, true, this.player2Finished)
+    else if (player2 == finishedPlayer) ArrangementCond(players, this.player1Finished, true)
+    else sys.error("Unexpected player finishes the arrangement")
+  }
+}
+
+case class ArrangementCond(players: Seq[String], player1Finished: Boolean = false, player2Finished: Boolean = false, random: Random = Random) extends ConditionalVertex[PlayerMove, ArrangementOp] {
+  override def predicate = player1Finished && player2Finished
+
+  override def output1: PlayerMove = PlayerMove(players, random.nextInt(1))
+
+  override def output2: ArrangementOp = ArrangementOp(players, player1Finished, player2Finished)
+}
+
+case class PlayerMove(players: Seq[String], currentPlayer: Int, deadShips: Seq[Int] = Seq(0, 0)) extends OperationVertex[Int, PlayerMoveResponseOp] {
+  def next(movingPlayer: Int): PlayerMoveResponseOp = {
+    if (movingPlayer != currentPlayer) sys.error("")
+
+    PlayerMoveResponseOp(players, deadShips, currentPlayer)
+  }
+}
+
+case class PlayerMoveResponseOp(players: Seq[String], deadShips: Seq[Int], currentPlayer: Int) extends OperationVertex[Boolean, PlayerMoveResponseCond] {
+  override def next(shipKilled: Boolean): PlayerMoveResponseCond = {
+    val opponentIndex = 1 - currentPlayer
+    val opponentDeadShips = deadShips(opponentIndex)
+    val deadShipDiff = if (shipKilled) 1 else 0
+    val newDeadShipCount = opponentDeadShips + deadShipDiff
+    val newDeadShips = deadShips.patch(opponentIndex, Seq(newDeadShipCount), 1)
+
+    PlayerMoveResponseCond(players, newDeadShips, currentPlayer)
+  }
+}
+
+case class PlayerMoveResponseCond(players: Seq[String], deadShips: Seq[Int], currentPlayer: Int) extends ConditionalVertex[GameEnd, PlayerMove] {
+  val opponentIndex = 1 - currentPlayer
+
+  override def predicate: Boolean = deadShips(opponentIndex) == 10
+
+  override def output1: GameEnd = GameEnd(players(currentPlayer))
+
+  override def output2: PlayerMove = PlayerMove(players, opponentIndex, deadShips)
+}
+
+case class GameEnd(winner: String) extends EndVertex
---
+++ test/fsa/StatesSpec.scala
@@ -0,0 +1,144 @@
+package fsa
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatestplus.play.PlaySpec
+
+import scala.util.Random
+
+@RunWith(classOf[JUnitRunner])
+class StatesSpec extends PlaySpec {
+
+  val players = Seq("vasya", "petya")
+
+  "Initial state" should {
+    "produce Player2Connection state" in {
+      InitialState.next("vasya") must be (Player2Connection("vasya"))
+    }
+  }
+
+  "Player2Connextion state" should {
+    "produce ArrangementOp state" in {
+      Player2Connection("vasya").next("petya") must be (ArrangementOp(Seq("vasya", "petya")))
+    }
+  }
+
+  "ArrangementOp state" should {
+    "produce ArrangementCond state if 1st player finishes arrangement first" in {
+      val initialState = ArrangementOp(players)
+      val expectedState = ArrangementCond(players, true, false)
+
+      initialState.next("vasya") must be (expectedState)
+    }
+
+    "produce ArrangementCond state if 2nd player finishes arrangement first" in {
+      val initialState = ArrangementOp(players)
+      val expectedState = ArrangementCond(players, false, true)
+
+      initialState.next("petya") must be (expectedState)
+    }
+
+    "produce ArrangementCond state if both players finished an arrangement" in {
+      val initialState = ArrangementOp(players, true)
+      val expectedState = ArrangementCond(players, true, true)
+
+      initialState.next("petya") must be (expectedState)
+    }
+
+    "throw an exception if invalid user name is used" in {
+      a [RuntimeException] must be thrownBy ArrangementOp(players).next("kolya")
+    }
+  }
+
+  "ArrangementCond state" should {
+    "produce ArrangementOp if only first user finished an arrangement" in {
+      val initialState = ArrangementCond(players, true, false)
+      val expectedState = ArrangementOp(players, true, false)
+
+      initialState.next must be (expectedState)
+    }
+
+    "produce ArrangementOp if only second user finished an arrangement" in {
+      val initialState = ArrangementCond(players, false, true)
+      val expectedState = ArrangementOp(players, false, true)
+
+      initialState.next must be (expectedState)
+    }
+
+    "produce PlayerMove state if both players finished an arrangement" in {
+      val random = new Random {
+        override def nextInt(n: Int): Int = 0
+      }
+      val initialState = ArrangementCond(players, true, true)
+      val expectedState = PlayerMove(players, 0)
+    }
+  }
+
+  "PlayerMove state" should {
+    "produce PlayerMoveResponseOp state" in {
+      val initialState = PlayerMove(players, 0)
+      val expectedState = PlayerMoveResponseOp(players, Seq(0, 0), 0)
+
+      initialState.next(0) must be (expectedState)
+    }
+
+    "throw an exception if wrong player tries to move" in {
+      val initialState = PlayerMove(players, 0)
+      a [RuntimeException] must be thrownBy initialState.next(1)
+    }
+  }
+
+  "PlayerMoveResponseOp state" should {
+    "produce PlayerMoveResponseCond state with same amount of ships if ship was not killed" in {
+      val initialState = PlayerMoveResponseOp(players, Seq(0, 0), 0)
+      val expectedState = PlayerMoveResponseCond(players, Seq(0, 0), 0)
+
+      initialState.next(false) must be (expectedState)
+    }
+
+    "produce PlayerMoveResponseCond with increased amount of ships if ship was killed by 1st player" in {
+      val initialState = PlayerMoveResponseOp(players, Seq(0, 0), 0)
+      val expectedState = PlayerMoveResponseCond(players, Seq(0, 1), 0)
+
+      initialState.next(true) must be (expectedState)
+    }
+
+    "produce PlayerMoveResponseCond with increased amount of ships if ship was killed by 2nd player" in {
+      val initialState = PlayerMoveResponseOp(players, Seq(0, 0), 1)
+      val expectedState = PlayerMoveResponseCond(players, Seq(1, 0), 1)
+
+      initialState.next(true) must be (expectedState)
+    }
+  }
+
+  "PlayerMoveResponseCond state" should {
+    "produce PlayerMove state if game is not over yet and it was 1st player's move" in {
+      val initialState = PlayerMoveResponseCond(players, Seq(0, 0), 0)
+      val expectedState = PlayerMove(players, 1, Seq(0, 0))
+
+      initialState.next must be (expectedState)
+    }
+
+    "produce PlayerMove state if game is not over yet and it was 2st player's move" in {
+      val initialState = PlayerMoveResponseCond(players, Seq(0, 0), 1)
+      val expectedState = PlayerMove(players, 0, Seq(0, 0))
+
+      initialState.next must be (expectedState)
+    }
+
+    "produce GameEnd state if game is over and it was 1st player's move" in {
+      val initialState = PlayerMoveResponseCond(players, Seq(9, 10), 0)
+      val expectedState = GameEnd("vasya")
+
+      initialState.next must be (expectedState)
+    }
+
+    "produce GameEnd state if game is over and it was 2st player's move" in {
+      val initialState = PlayerMoveResponseCond(players, Seq(10, 9), 1)
+      val expectedState = GameEnd("petya")
+
+      initialState.next must be (expectedState)
+    }
+  }
+
+}
