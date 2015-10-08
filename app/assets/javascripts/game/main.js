fe33947cae7e348bd0797f33bc36b87fc229e5fb
ig <ig@anahoret.com>
2015-10-08 17:43:35 +0300

    Create framework for front-end game implementation.

---
+++ app/assets/javascripts/game/main.js
@@ -0,0 +1,9 @@
+import arrangement from './arrangement_step/main.js';
+import connection from './connection_step/main.js';
+import gameOver from './game_over_step/main.js';
+import own_move from './own_move_step/main.js';
+import rival_move from './rival_move_step/main.js';
+
+export default function () {
+
+}
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,1 @@
-import arrangement from './game_field/arrangement/main.js';
-import game from './game_field/game/main.js';
-
-arrangement();
-game();
+import game from './game/main.js';
